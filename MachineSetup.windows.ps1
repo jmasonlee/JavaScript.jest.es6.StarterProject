b45ff33ae8844a684c68facf2a49f650f8f79e12
jmasonlee <jmasonlee@gmail.com>
2021-01-26 18:00:01 -0700

    Update MachineSetup.windows.ps1

--- MachineSetup.windows.ps1
+++ MachineSetup.windows.ps1
@@ -8,5 +8,7 @@ iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/javascr
 # Clone repo
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/jmasonlee/JavaScript.jest.es6.StarterProject.git C:\Code\JavaScript.jest.es6.StarterProject
 cd C:\Code\JavaScript.jest.es6.StarterProject
+# begin-snippet: add_es6
 yarn add jest @babel/cli @babel/core @babel/preset-env regenerator-runtime
 yarn test 
+# end-snippet: add_es6
